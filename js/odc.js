fcdc4755292f8994c2f8e3ca2c7dd4603d247646
Rilind Pozhegu <Rilind Pozhegu>
2016-11-03 14:49:31 +0100

    homepage done + responsive

---
+++ js/odc.js
@@ -0,0 +1,39 @@
+    	// Testimonials
+
+    	$(document).ready(function() {
+            $("#owl-demo0").owlCarousel({
+                autoPlay: 4000,
+                items: 1,
+                itemsDesktop: [1199, 3],
+                itemsDesktopSmall: [979, 3]
+            });
+
+        });
+
+
+        // Lastest news
+
+        $(document).ready(function() {
+            $("#owl-demo1").owlCarousel({
+                autoPlay: 3500,
+                items: 3,
+                itemsDesktop: [1199, 3],
+                itemsDesktopSmall: [979, 3]
+            });
+
+        });
+
+
+
+        // Lastest From Galery
+        $(document).ready(function() {
+            $("#owl-demo").owlCarousel({
+                autoPlay: 3500,
+                items: 3,
+                itemsDesktop: [1199, 3],
+                itemsDesktopSmall: [979, 3]
+            });
+
+        });
+
+
